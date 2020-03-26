2a4a8e9d3a338c36316903fdaf6cc0187437afe8
lyhuuthanh <lyhuuthanhtv@gmail.com>
2020-03-26 11:34:29 +0700

    anh xa java

--- app/src/main/java/com/example/baitaploto2/MainActivity.java
+++ app/src/main/java/com/example/baitaploto2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.baitaploto2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+// khai bao
+    EditText mnhapsomin, mnhapsomax;
+    Button mrandom, mchoilai;
+    TextView mketqua;
+    String chuoisomin, chuoisomax;
+    int xuatsomin, xuatsomax;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //anh xa
+        mnhapsomin = findViewById(R.id.nhapsomin);
+        mnhapsomax = findViewById(R.id.nhapsomax);
+        mrandom = findViewById(R.id.random);
+        mchoilai = findViewById(R.id.choilai);
+        mketqua = findViewById(R.id.Ketqua);
+
     }
 }
